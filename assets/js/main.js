adcde8ff2d226e4012233a4030387db7bd2282da
André <122179908+XxKillerzinxX03@users.noreply.github.com>
2023-06-14 13:34:17 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -68,6 +68,8 @@ function updateProfessionalExperience(profileData) {
         return `
             <li>
                 <h3 class="title">${experience.name}</h3>
+                <p class="period">${experience.period}</p>
+                <p>${experience.description}</p>
             </li>
         `
     }).join('')
